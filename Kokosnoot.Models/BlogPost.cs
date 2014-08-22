590cbe734d1deb2b338d03b42e22efaa0e83c3fd
snald <niels.abels@gmail.com>
2014-08-23 01:55:33 +0200

    added Raven embedded

---
+++ Kokosnoot.Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kokosnoot.Models
+{
+    public class BlogPost
+    {
+    }
+}
