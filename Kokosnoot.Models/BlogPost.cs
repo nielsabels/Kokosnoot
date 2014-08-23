fad4fac654a9fd12b396113a482591f87f11b035
snald <niels.abels@gmail.com>
2014-08-23 02:18:45 +0200

    added blogpost controller/views

--- Kokosnoot.Models/BlogPost.cs
+++ Kokosnoot.Models/BlogPost.cs
@@ -8,5 +8,8 @@ namespace Kokosnoot.Models
 {
     public class BlogPost
     {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Footer { get; set; }
     }
 }
---
+++ Kokosnoot/Controllers/BlogPostController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kokosnoot.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        // GET: BlogPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BlogPost/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BlogPost/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogPost/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlogPost/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BlogPost/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlogPost/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BlogPost/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
