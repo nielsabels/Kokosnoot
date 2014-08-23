8ae4a3cffe1ebfd37ffaabb123aae60c2e236345
snald <niels.abels@gmail.com>
2014-08-23 03:12:47 +0200

    moved projects around in solution

---
+++ Kokosnoot.Services/BlogPostService.cs
@@ -0,0 +1,39 @@
+﻿using Kokosnoot.Models;
+using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kokosnoot.Services
+{
+    public class BlogPostService
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public BlogPostService(IDocumentStore documentStore)
+        {
+            _documentStore = documentStore; 
+        }
+
+        public void CreateAnyBlogPost()
+        {
+            using (var session = _documentStore.OpenSession())
+            {
+                var blogPostId = "BlogPosts/1";
+                var blogPost = session.Load<BlogPost>(blogPostId);
+
+                if (blogPost == null)
+                {
+                    blogPost = new BlogPost();
+                    blogPost.Id = blogPostId;
+                    blogPost.Title = "Titel";
+
+                    session.Store(blogPost);
+                    session.SaveChanges();
+                }
+            }
+        }
+    }
+}
