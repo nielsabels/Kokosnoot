1fb1eda923fd816c00b96e1c93bb7c7d81bf64d9
snald <niels.abels@gmail.com>
2014-08-24 16:59:07 +0200

    added ekko lightbox; updated sample post

--- Kokosnoot/App_Start/BundleConfig.cs
+++ Kokosnoot/App_Start/BundleConfig.cs
@@ -23,9 +23,13 @@ namespace Kokosnoot
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/lightbox").Include(
+                      "~/Scripts/ekko-lightbox.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/ekko-lightbox.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
