c5ec30b7b2f297460d92d8b61367927c196d5ef5
snald <niels.abels@gmail.com>
2014-11-12 22:00:27 +0100

    date of blogpost now deeplinks to blogpost

---
+++ Kokosnoot/Extensions/DocumentExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kokosnoot.Models.Persistence;
+
+namespace Kokosnoot.Extensions
+{
+    public static class DocumentExtensions
+    {
+        public static string MvcDocumentId(this Document document)
+        {
+            return document.Id.Split('/').LastOrDefault();
+        }
+
+    }
+}
